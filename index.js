1b14337d1591bca57676caee8f8587b7d91d645b
Lucas <lucasfreiresabino@gmail.com>
2022-11-15 11:02:23 -0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function vaporcode(string) {
-    let arr = string.split('')
-    let result = ''
-    console.log(arr)
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] !== ' ' && i !== arr.length - 1){
-            result += arr[i].toUpperCase() + '  '
-            console.log(result)
-            
-        } else if(arr[i] !== ' ' && i === arr.length - 1) {
+  let arr = string.split('')
+  let result = ''
+//     console.log(arr)
+  for (let i = 0; i < arr.length; i++) {
+      if(arr[i] !== ' ' && i === arr.length - 2 && arr[i + 1] === ' '){
           result += arr[i].toUpperCase()
-        }
+//             console.log(result)
+          
+      } else if (arr[i] !== ' ' && i !== arr.length - 1) {
+        
+        result += arr[i].toUpperCase() + '  '
+      }else if(arr[i] !== ' ' && i === arr.length - 1) {
+        result += arr[i].toUpperCase()
+      }
 
-    }
-  return result
   }
+return result
+}
 
 
   vaporcode('Let get some bananas')
